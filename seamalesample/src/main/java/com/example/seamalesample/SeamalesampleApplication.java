bde67cec92af121a942f946b20d4a59eee430c22
nogiharu <nogiharu@icloud.com>
2022-03-14 18:07:50 +0900

    First Commit

---
+++ seamalesample/src/main/java/com/example/seamalesample/SeamalesampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.seamalesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamalesampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamalesampleApplication.class, args);
+	}
+
+}
---
+++ seamalesample/src/test/java/com/example/seamalesample/SeamalesampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seamalesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeamalesampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
